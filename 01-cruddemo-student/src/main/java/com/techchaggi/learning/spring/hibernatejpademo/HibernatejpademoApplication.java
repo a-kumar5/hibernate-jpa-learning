18789c4d9a81825f4b40f103262f6dabc73bc50b
a-kumar5 <ayush.1306.kumar@gmail.com>
2023-08-16 10:09:20 +0530

    First basic commit

---
+++ 01-cruddemo-student/src/main/java/com/techchaggi/learning/spring/hibernatejpademo/HibernatejpademoApplication.java
@@ -0,0 +1,13 @@
+package com.techchaggi.learning.spring.hibernatejpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatejpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatejpademoApplication.class, args);
+	}
+
+}
---
+++ 01-cruddemo-student/src/test/java/com/techchaggi/learning/spring/hibernatejpademo/HibernatejpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchaggi.learning.spring.hibernatejpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatejpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
